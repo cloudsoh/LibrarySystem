d834950b79051fdfbd0711c7ea7906aec4ec500c
Soh Jun Fu <cloudsoh03021@gmail.com>
2016-10-31 04:30:57 +0800

    adminoverview bug

--- message.php
+++ message.php
@@ -1,3 +1,15 @@
 <?php
+require_once('config.php');
+date_default_timezone_set("Asia/Singapore");//set timezone
+$currenttime=date("Y-m-d H:i:s");
+echo $currenttime;
+// $DTcurrenttime=DateTime::createFromFormat('Y-m-d H:i:sa', $currenttime);
 
-$sql = "INSERT INTO message(message,date) VALUES ('$message','$date')";
+$message=$_POST['message'];
+$sql = "INSERT INTO feedback(content,date) VALUES ('$message','$currenttime')";
+if($conn->query($sql)===TRUE){
+	echo "<script>alert('Feedback Successfully Submitted!!');window.location.href='index.php';</script>";
+}else{
+	echo "Error: ".$sql. "<br>" .$conn->error;
+	echo "<script>alert('Fail submit!!');</script>";
+}
