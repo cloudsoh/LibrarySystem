32afc429f47b77395a8f4635b38d08fdbceb588e
zihengChng <chngzi1@outlook.com>
2016-10-30 21:33:13 +0800

    ignore it

--- insertbook.php
+++ insertbook.php
@@ -1,3 +1,8 @@
 <?php 
 require_once('config.php');
+
+$bookName=mysqli_real_escape_string($conn,$_POST['bookName']);
+$author=mysqli_real_escape_string($conn,$_POST['author']);
+$publisher=mysqli_real_escape_string($conn,$_POST['publisher']);
+$genre=mysqli_real_escape_string($conn,$_POST['genre']);
  ?>
