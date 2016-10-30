842f72430d040143e36e9f9eefbae38b7e9ab636
Soh Jun Fu <cloudsoh03021@gmail.com>
2016-10-31 05:05:51 +0800

    removeMessage.php

---
+++ removeMessage.php
@@ -0,0 +1,15 @@
+<?php 
+require_once('config.php');
+include('session.php');
+$deleteID=$_GET['id'];
+$id=$_SESSION['userid'];
+$sql="DELETE FROM feedback WHERE id = $deleteID";
+echo $sql;
+if($conn->query($sql)===TRUE){
+	echo "<script>alert('Successfully Removed.');window.location='studentoverview.php'</script>";
+}else{
+	echo "<script>alert('Seem like some error to delete it.');</script>";
+}
+//window.location='adminbooklist.php
+$conn->close();
+ ?>
